596be501e4703a71344d1b6b8d934059812dbb2f
RitujaSinha <109161939+RitujaSinha@users.noreply.github.com>
2022-08-22 01:36:11 +0530

    EntityFramework Assign

---
+++ EntityFrameworkAssign/GlobalVariable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace EntityFramework
+{
+    public static class GlobalVariable
+    {
+        public static HttpClient WebApiClient = new HttpClient();
+
+        static GlobalVariable()
+        {
+            WebApiClient.BaseAddress = new Uri("https://localhost:44379//api/");
+            WebApiClient.DefaultRequestHeaders.Clear();
+            WebApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
